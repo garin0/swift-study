9210614798529ea1e147286842e95903503f7c94
garin <gairn0112@naver.com>
2016-01-18 16:29:05 +0900

    2

--- UserLoginAndRegisterExampleInSwift-master-2/UserLoginAndRegistration/ViewController.swift
+++ UserLoginAndRegisterExampleInSwift-master-2/UserLoginAndRegistration/ViewController.swift
@@ -10,15 +10,12 @@ import UIKit
 import Parse
 
 class ViewController: UIViewController {
+    
 
-    
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
- 
+        
+       
     }
     
 
